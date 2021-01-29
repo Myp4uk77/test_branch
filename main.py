0d5f43327c4784ddb22836fbe039f771949ee222
Myp4uk <mailformyiphone@ukr.net>
2021-01-29 20:26:41 +0200

    'print'

---
+++ main.py
@@ -0,0 +1,35 @@
+# x = 2
+# if x > 0:
+#     y = x ** 0.5
+# else:
+#     y = x ** 2
+# print(y)
+
+# print("""Menu:
+# 1. File
+# 2. View
+# 3. Exit""")
+# choice = input("Enter your choice: ")
+# if choice == "1":
+#     print("File")
+# elif choice == "2":
+#     print("View")
+# elif choice == "3":
+#     print("Exit")
+# else:
+#     print("Incorrect choice")
+
+# is_ready = False
+# state_msg = "Ready"
+# if is_ready == True
+# else: "Not ready yet"
+# print(state_msg)
+
+
+# str1 = "First line of text"
+# str2 = "Second line of text"
+# string = (str1, str2)
+# print(string)
+
+
+print('hi')
